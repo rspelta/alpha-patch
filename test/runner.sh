f050e571cc472e01f1bab268bd450122d540b93a
rspelta <architech@architech.(none)>
2014-04-17 17:04:54 +0200

    added runner.sh

---
+++ test/runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "start PID=${1}"
+
+LOOP=1
+
+while [ ${LOOP} -eq 1 ] 
+do
+	sleep 5
+	STATUS=`ps -p ${1} | grep ${1}`
+	[ $? -eq 0 ] || { LOOP=0;  }
+	echo -n "."
+done
+
+echo "finish"
